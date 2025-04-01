b821643e1f931abde41b2b909d2ff3d9bbd57d2c
Jim Finnis <jim.finnis@gmail.com>
2025-04-01 14:56:45 +0100

    Annotations are working and I can add them.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import MapComponent from './Map'
+import {MapComponent} from './Map'
+import {Annotorious} from "@annotorious/react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <MapComponent></MapComponent>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Annotorious>
+      <h1>Test App</h1>
+        <MapComponent/>
+    </Annotorious>
   )
 }
 
--- src/Map.tsx
+++ src/Map.tsx
@@ -1,34 +1,99 @@
-import OpenSeadragon, {Options} from "openseadragon"
-import {ReactNode, useEffect} from "react";
+import {
+    AnnotoriousOpenSeadragonAnnotator,
+    OpenSeadragonAnnotator,
+    OpenSeadragonViewer,
+    useAnnotator
+} from '@annotorious/react';
 
-export default function MapComponent (): ReactNode {
-    // useEffect lets you declare things that happen when the component is added to the DOM.
-    // It takes two args: the setup func itself (which is void) and dependencies, which are
-    // "reactive values" (state) referenced inside the setup code.
+import '@annotorious/react/annotorious-react.css';
+import {Options} from "openseadragon";
+import {useEffect, useState} from "react";
+
+// The function is (i.e. returns a) component. The component is just a div,
+// but when it's added to the DOM an "effect hook" runs that loads up OpenSeadragon
+// and sets it to view the div we returned.
+
+export const MapComponent = () => {
+    const source = {
+        type: "image",
+        url: "src/assets/perseverance.jpg"
+    }
+    const [tool, setTool] = useState<'rectangle' | 'polygon' | undefined>();
+
+    const options: Options = {
+        "id": "seadragon-viewer",
+        prefixUrl: "//openseadragon.github.io/openseadragon/images/",
+        zoomPerClick: 1,    // this will make sure we don't zoom in automatically on click
+        "tileSources": source
+    }
+
+    const anno = useAnnotator<AnnotoriousOpenSeadragonAnnotator>();
+
+    // this does the initial setup when the annotator is mounted.
     useEffect(() => {
+        // anno might be null because the component is not mounted yet.
+        // This function will run again when the component mounts
+        if (!anno) {
+            return;
+        }
+        anno.on('createAnnotation', () => {setTool(undefined)})
 
-        const source = {
-            type: "image",
-            url: "src/assets/perseverance.jpg"
+        // we can set the initial annotations here.
+        const annots = localStorage.getItem('annotations');
+        if (annots) {
+            anno.setAnnotations(JSON.parse(annots));
         }
 
-        const options: Options = {
-            "id": "seadragon-viewer",
-             prefixUrl: "//openseadragon.github.io/openseadragon/images/",
-            "tileSources": source
-        }
+        // anno.setAnnotations(
+        //     [createTestAnno(100, 100, 20, 20, "foo")]
+        // )
+    }, [anno]);
 
-        const viewer = OpenSeadragon(options)
 
-        // useEffect returns a cleanup function.
-        return () => {
-            viewer.destroy()
-        }
-    }, []);
-
+    // and now we return the actual DOM elements that will be mounted.
     return (
-        // you have to give the container a size or the viewer won't be visible!
-        <div style={{ width:"800px",  height:"600px" }}
-            id="seadragon-viewer" className="seadragon-viewer" />
+        <div>
+            <OpenSeadragonAnnotator
+                drawingEnabled={tool != undefined}
+
+                tool={tool || 'rectangle'}
+            >
+                <OpenSeadragonViewer className="osd-container" options={options}/>
+                <button onClick={() => {console.log(anno.getAnnotations())}}>
+                    Display annotations on console
+                </button>
+            </OpenSeadragonAnnotator>
+            <p>Click a button and then draw your annotation.</p>
+            <div className='drawmodes'>
+                <button
+                    className={tool === 'rectangle' ? 'button-active' : ''}
+                    onClick={() => {
+                    setTool('rectangle');
+                }}>Rectangle</button>
+                <button
+                    className={tool === 'polygon' ? 'button-active' : ''}
+                    onClick={() => {
+                    setTool('polygon');
+                }}>Polygon</button>
+            </div>
+
+            { /* These are other buttons not related to drawing modes*/ }
+
+            <div className='actions'>
+                <button
+                    onClick={() => {
+                        // save annotations
+                        const annots = anno.getAnnotations();
+                        localStorage.setItem('annotations', JSON.stringify(annots));
+                    }}>Save</button>
+                <button
+                    onClick={() => {
+                        // clear annotations with confirmation
+                        if (confirm('Are you sure you want to clear all annotations?')) {
+                            anno.clearAnnotations();
+                        }
+                    }}>Clear</button>
+            </div>
+        </div>
     );
 }
